b857fa93ec15d1c88957c7f98da446e53cbcbd57
holsteco <39134537+holsteco@users.noreply.github.com>
2018-09-12 22:01:45 -0400

    updates

---
+++ Code/quotient_and_remainder.py
@@ -0,0 +1,23 @@
+#The code below tries to calculate the quotient and remainder
+#of a pair of numbers. However, right now None is printing
+#in several places throughout the code's execution. Fix the
+#code so that it behaves as intended: it should print the
+#quotient, remainder, then the message, "We're done!"
+#
+#HINT: There are a couple ways to do this, and some of those
+#ways might be a little advanced right now. Note that 17 // 6
+#will correctly calculate the quotient and num_1 % num_2 will
+#correctly calculate the remainder.
+#
+#HINT 2: Click Reset to start over from scratch.
+
+num_1 = 17
+num_2 = 6
+
+quotient = num_1 // num_2
+print("The quotient is", quotient)
+
+remainder =  num_1 % num_2
+print("The remainder is", remainder)
+
+print("We're done!")
