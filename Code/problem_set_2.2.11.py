2721a7b2062c92af5d2663544b7a5fbdeb5c4f10
holsteco <39134537+holsteco@users.noreply.github.com>
2018-09-18 06:52:52 -0400

    updates

---
+++ Code/problem_set_2.2.11.py
@@ -0,0 +1,10 @@
+#corey b. holstege
+#2018-09-18
+#problem_set_2.2.11
+
+userInput = input("Enter a number: ")						#line 1
+print("Your original input: " + userInput)					#line 2
+userInputAsFloat = float(userInput)							#line 3
+print("Your input as a float:", userInputAsFloat)		#line 4
+userInputAsInt = int(userInput)								#line 5
+print("Your input as an integer:", userInputAsInt)	#line 6
---
+++ Code/problem_set_2.2.8.py
@@ -0,0 +1,18 @@
+#corey b. holstege
+#2018-09-18
+#problem_set_2.2.8
+
+myInteger = 3
+myFloat = 5.5
+myString = "Hello!"
+myBoolean = False
+a = myBoolean + myInteger
+b = myInteger * myString
+c = myFloat * myBoolean
+d = myFloat * myInteger
+
+#print
+print("a:" , a)
+print("b:" , b)
+print("c:" , c)
+print("d:" , d)
