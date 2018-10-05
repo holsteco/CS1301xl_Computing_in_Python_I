d024c8c87de7450cd9e6c069b2f33b9620d1fa8c
holsteco <39134537+holsteco@users.noreply.github.com>
2018-10-05 06:54:58 -0400

    updates

---
+++ Code/2.4.4.3_letter_count.py
@@ -0,0 +1,12 @@
+#corey b holstege
+#2018-10-05
+#
+
+
+letter_count = 0
+word_word = input("enter a word: ")
+
+for character in word_word:
+	letter_count += 1
+
+print(letter_count)
---
+++ Code/2.4.5.1_CheckIfaTriangleExists.py
@@ -0,0 +1,11 @@
+#corey b holstege
+#2018-10-05
+#
+
+side_1 = int(input("Enter the shortest side: "))
+side_2 = int(input("Enter the next shortest side: "))
+side_3 = int(input("Enter the longest side: "))
+
+result = (side_1 + side_2) > side_3
+
+print("These sides can form a triangle is:", result)
